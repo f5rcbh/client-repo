acfc3305e62136c4073013c95463c6da8d0b3778
f5rcbh <f5rcbh@github.com>
2023-07-27 15:17:32 +0300

    info page changing

---
+++ info.js
@@ -0,0 +1,20 @@
+// const nextPage = document.querySelector('#next')
+// const degree = document.querySelector('.certificate-info')
+// const form = document.querySelector('form')
+// const FInfo = document.querySelector('.first-info')
+// const certificate = document.querySelector('.certificate-info')
+
+
+
+// console.log(certificate)
+
+// nextPage.addEventListener('click',(e)=>{
+//     e.preventDefault()
+    
+    
+    
+// })
+
+
+
+// something wrong ill do it after i finish the whole page
